775171c0adb3f3c1ba1cc0e13784aea5d0d07fb9
jd <jukic-dejan@hotmail.com>
2015-07-22 23:00:19 +0200

    calendar ajax call works

---
+++ src/main/java/com/forall/laundry/controller/DateController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.forall.laundry.controller;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.inject.Named;
+import org.primefaces.event.SelectEvent;
+
+/**
+ *
+ * @author jd
+ */
+@Named
+public class DateController implements Serializable{
+    
+    public void update(SelectEvent event){
+        
+        Date date = (Date) event.getObject();
+        System.out.println(date);
+        
+    }
+    
+}
