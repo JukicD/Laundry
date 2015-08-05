780c4bc99d111be431e34e44f7c0caf10ef789a0
jd <jukic-dejan@hotmail.com>
2015-08-05 15:08:40 +0200

    working linechart example

--- src/main/java/com/forall/laundry/controller/statistic/StatisticController.java
+++ src/main/java/com/forall/laundry/controller/statistic/StatisticController.java
@@ -7,15 +7,15 @@ package com.forall.laundry.controller.statistic;
 
 import com.forall.laundry.service.StatisticService;
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.Calendar;
 import java.util.Date;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import org.primefaces.model.chart.BarChartModel;
-import org.primefaces.model.chart.BarChartSeries;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.LineChartModel;
 
 /**
  *
@@ -32,7 +32,7 @@ public class StatisticController implements Serializable{
     
     private Date to;
     
-    private BarChartModel model;
+    private LineChartModel model;
     
     private boolean entered;
 
@@ -63,34 +63,41 @@ public class StatisticController implements Serializable{
 
     public void createLineChart(){
         
-        model = new BarChartModel();
-        Calendar calFrom = Calendar.getInstance();
-        calFrom.setTime(from);
-        int monthFrom = calFrom.get(Calendar.MONTH);
-        int yearFrom = calFrom.get(Calendar.YEAR);
+        model = new LineChartModel();
+        ChartSeries boys = new ChartSeries();
+        boys.setLabel("Boys");
+        boys.set("2004", 120);
+        boys.set("2005", 100);
+        boys.set("2006", 44);
+        boys.set("2007", 150);
+        boys.set("2008", 25);
+ 
+        ChartSeries girls = new ChartSeries();
+        girls.setLabel("Girls");
+        girls.set("2004", 52);
+        girls.set("2005", 60);
+        girls.set("2006", 110);
+        girls.set("2007", 90);
+        girls.set("2008", 120);
         
-        Calendar calTo = Calendar.getInstance();
-        calTo.setTime(to);
-        int monthTo = calTo.get(Calendar.MONTH);
-        int yearTo = calTo.get(Calendar.YEAR);
-        
-        
-        BarChartSeries series = new BarChartSeries();
-        System.out.println(monthFrom + " " + monthTo);
-        for(int i = monthFrom; i <= monthTo; i++){
-            
-            BigDecimal monthSum = statisticService.getSumFromMonth(i, yearTo);
-            System.out.println(monthSum.setScale(2, RoundingMode.HALF_UP));
-            series.set(calFrom.getTime(), monthSum.setScale(2, RoundingMode.HALF_UP));
-        }
-       model.addSeries(series);
+        model.setTitle("Category Chart");
+        model.setLegendPosition("e");
+        model.setShowPointLabels(true);
+        model.getAxes().put(AxisType.X, new CategoryAxis("Years"));
+        Axis yAxis = model.getAxis(AxisType.Y);
+        yAxis.setLabel("Births");
+        yAxis.setMin(0);
+        yAxis.setMax(200);
+        model.addSeries(boys);
+        model.addSeries(girls);
+       
     }
 
-    public BarChartModel getModel() {
+    public LineChartModel getModel() {
         return model;
     }
 
-    public void setModel(BarChartModel model) {
+    public void setModel(LineChartModel model) {
         this.model = model;
     }
 }
